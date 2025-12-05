3d370512c40fb3aad3dbcc3117d1f70ac4a2933e
Brandon <brandon@parksbra.com>
2025-12-04 17:05:47 -0800

    add better version def

--- terraform/modules/_haenv/versions.tf
+++ terraform/modules/_haenv/versions.tf
@@ -2,19 +2,19 @@ terraform {
   required_providers {
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = "~> 2"
+      version = "~> 2.0"
     }
     kubectl = {
       source = "alekc/kubectl"
-      version = "~> 2"
+      version = "~> 2.0"
     }
     helm = {
       source = "hashicorp/helm"
-      version = "~> 3"
+      version = "~> 3.0"
     }
     jinja = {
       source = "NikolaLohinski/jinja"
-      version = "~> 2"
+      version = "~> 2.0"
     }
   }
 }
