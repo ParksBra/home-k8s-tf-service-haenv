eb67c14f419b5d4d5aab40ff715a3c6c91017cad
Brandon <brandon@parksbra.com>
2025-12-08 16:30:29 -0800

    remove ado provider

--- terraform/data_sources.tf
+++ terraform/data_sources.tf
@@ -11,8 +11,3 @@ data "kubernetes_config_map" "storage_context" {
     namespace = "platform-storage"
   }
 }
-
-data "azuredevops_variable_group" "library" {
-  project_id = var.azuredevops_library_project_id
-  name       = var.azuredevops_library_name
-}
